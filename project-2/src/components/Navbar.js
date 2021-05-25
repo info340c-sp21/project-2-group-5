55f6ce445ba82a2c1ded0b3c5cd419e361801bf5
hongyt <hongyt@uw.edu>
2021-05-25 10:59:10 -0700

    page icon

--- project-2/src/components/Navbar.js
+++ project-2/src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
           <nav className= "navbar">
               <div className="navbar-container">
                 <Link to="/" className="navbar-logo" onClick={closeMenu}>
-                    Grenville<i className='fas fa-leaf' />
+                    Grenville <i className='fas fa-leaf' />
                 </Link>
                 <div className= 'menu-icon' onClick={handleClick}>
                   <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
