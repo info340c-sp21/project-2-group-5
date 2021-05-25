08f2419b12ab297c6e8afc97e3effe2c52fc3eab
hongyt <hongyt@uw.edu>
2021-05-24 17:26:10 -0700

    nav bar fix2

--- project-2/src/components/Navbar.js
+++ project-2/src/components/Navbar.js
@@ -1,28 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { Button } from './Button';
-
 function Navbar() {
 
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true)
 
     const handleClick = () => setClick(!click);
 
-    const closeMenu = () => setClick(false);
-
-    const showButton = () => {
-      if(window.innerWidth <= 960) {
-        setButton(false);
-      } else {
-        setButton(true);
-      }
-    }
-
-    useEffect(() => { showButton();}, []);
-
-    window.addEventListener('resize', showButton);
+    const closeMenu = () => setClick(false);;
 
     return (
           <nav className= "navbar">
@@ -33,6 +18,7 @@ function Navbar() {
                 <div className= 'menu-icon' onClick={handleClick}>
                   <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                 </div>
+                <div className= 'nav-option'>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                   <li className='nav-item'>
                     <Link to='/Calculator' className='nav-links' onClick={closeMenu}>
@@ -54,13 +40,8 @@ function Navbar() {
                       Feature TBD
                     </Link>
                   </li>
-                  <li className='nav-item'>
-                    <Link to='/sign-up' className='nav-links-mobile' onClick={closeMenu}>
-                      Sign In
-                    </Link>
-                  </li>
                 </ul>
-                {button && <Button buttonStyle='btn--outline'>SIGN IN </Button>}
+                </div>
               </div>
           </nav>
     )
