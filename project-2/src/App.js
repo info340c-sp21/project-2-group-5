2e7cdab0c787632c557ab2876599414cc74c630a
Yenni Lee <yennilee@uw.edu>
2021-05-26 22:01:09 -0700

    added elements on comparison page

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/HomePage'
 import Tips from './components/pages/Tips'
 import Footer from './components/footer';
+import Comparison from './components/pages/Comparison';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home}/>
+        <Route path='/Comparison' exact component={Comparison}/>
         <Route path='/Pro-Tips' exact component={Tips}/>
       </Switch>
       <Footer />
--- project-2/src/components/pages/Comparison.js
+++ project-2/src/components/pages/Comparison.js
@@ -1,1 +1,61 @@
 //The page of the Comparison goes here
+
+import React from 'react';
+import '../../App.css';
+import './Comparison.css';
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import Emissions from '../../data/owid-co2-data'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+
+function Comparison() {
+    return (
+      <div>
+        <h1>Compare emissions with other countries</h1>
+        <div>
+            <div id='comparison-chart'>
+                <Chart />
+            </div>
+            <div id='dropdown'>
+                <Options />
+            </div>
+        </div>
+      </div>
+    );
+}
+
+export default Comparison;
+
+function Chart() {
+    return (
+        <XYPlot
+        width={300}
+        height={300}>
+        <HorizontalGridLines />
+        <LineSeries
+            data={[
+            {x: 1, y: 10},
+            {x: 2, y: 5},
+            {x: 3, y: 15}
+        ]}/>
+        <XAxis />
+        <YAxis />
+        </XYPlot>
+    )
+}
+
+export class Options extends React.Component {
+    render(){
+        const countries = Object.keys(Emissions);
+        const defaultOption = countries[0];
+        return (
+              <Dropdown options={countries} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+        )
+    }
+}
+
+
+console.log(Emissions.Afghanistan);
+
+console.log(Emissions.Afghanistan.data[0]);
